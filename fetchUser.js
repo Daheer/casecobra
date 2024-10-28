e060cb21ea9cfd83f23e1b8da7523fe717d9ff25
Daheer <suhayrid6@gmail.com>
2024-10-28 21:58:13 +0100

    Initial commit

--- fetchUser.js
+++ fetchUser.js
@@ -1,19 +1,27 @@
-const headers = {
-  'Accept': 'application/json',
-  'Authorization': 'Bearer cvPkfEl3czvdBPYxguTPlOUZ2HuXlAGkHG0KYHg95HPHWXwbLdrhe' // Replace with your access token
-};
+async function getToken() {
+  try {
+    const response = await fetch(`https://casecobra.kinde.com/oauth2/token`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        audience: "https://casecobra4deedax.kinde.com/api",
+        grant_type: "client_credentials",
+        client_id: "3c00eeb55a974988b4a71183220f0c40",
+        client_secret: "cvPkfEl3czvdBPYxguTPlOUZ2HuXlAGkHG0KYHg95HPHWXwbLdrhe"
+      })
+    });
 
-fetch('https://casecobra4deedax.kinde.com/oauth2/user_profile', { // Replace with your subdomain
-  method: 'GET',
-  headers: headers
-})
-.then(function(res) {
-  return res.json();
-})
-.then(function(body) {
-  console.log(body);
-})
-.catch(function(error) {
-  console.error('Error:', error);
-});
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
 
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getToken();
