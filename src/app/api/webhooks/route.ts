26fab0f4cb285d64f94574139e155f6490829889
Daheer <suhayrid6@gmail.com>
2024-10-29 15:00:36 +0100

    Initial commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
       }
 
       const billingAddress = session.customer_details!.address
-      const shippingAddress = session.shipping!.address
+      const shippingAddress = session.shipping_details!.address
 
       await db.order.update({
         where: { id: orderId },
