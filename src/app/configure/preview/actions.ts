6ef41b6188800c9da89d72bb34ee12dc9130af2a
Daheer <suhayrid6@gmail.com>
2024-10-29 13:55:42 +0100

    Initial commit

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -16,6 +16,8 @@ export const createCheckoutSession = async ({configId}: {configId: string}) => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
+  console.log("USER FOR ACTIONS", user)
+
   if (!user) { throw new Error("You need to be logged in")}
 
   const { finish, material } = configuration
