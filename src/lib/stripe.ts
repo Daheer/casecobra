dbe17b3b53513681eaf40ca043b4c9d672838618
Daheer <suhayrid6@gmail.com>
2024-10-29 15:41:20 +0100

    Initial commit

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
   // @ts-ignore
-  apiVersion: "2020-08-27",
+  apiVersion: "2024-09-30.acacia",
   typescript: true,
 })
