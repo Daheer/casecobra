a2ea65b995f3be8395d73694bdeb2d2f9cf03c15
Daheer <suhayrid6@gmail.com>
2024-10-29 13:42:46 +0100

    Initial commit

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -131,7 +131,7 @@ const DesignPreview = async ({ configuration }: { configuration: Configuration }
               </div>
             </div>
             <div className="mt-8 flex justify-end pb-12">
-              <Button onClick={handleCheckout} className="px-4 sm:px-6 lg:px-8">Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" /></Button>
+              <Button onClick={() => handleCheckout()} className="px-4 sm:px-6 lg:px-8">Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" /></Button>
             </div>
           </div>
         </div>
